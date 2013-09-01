a09502b4db384f51aa8be5a6a801a05e72f02781
Shivkumar Shivaji <sshivaji@gmail.com>
2013-08-31 22:24:57 -0700

    Revert "Small fixes"
    This reverts commit 9012c884c4b83edd446a02d7961278793807b9c9.

--- kivy_util.py
+++ kivy_util.py
@@ -2,7 +2,6 @@ from kivy.resources import resource_find
 from kivy.uix.label import Label
 from kivy.uix.scrollview import ScrollView
 from kivy.graphics import Rectangle
-from kivy.uix.image import Image
 
 class ScrollableLabel(ScrollView):
     def __init__(self, text, ref_callback=None, *args, **kwargs):
@@ -28,28 +27,3 @@ class ScrollableLabel(ScrollView):
         self.background.size = size
         self.label.text_size = (size[0]-50, None)
 
-
-class ImageWithBackground(Image):
-    def __init__(self, ref_callback=None, *args, **kwargs):
-        super(Image, self).__init__(*args, **kwargs)
-        with self.canvas:
-            self.background = Rectangle(source=resource_find("img/panel.png"), size_hint=(1,1))
-#        self.image = Image()
-#        self.label=Label(text="", size_hint_y=None, pos_hint={'x':0.1, 'y':0.1})
-#        self.label.bind(texture_size=self._set_summary_height, on_ref_press=ref_callback)
-#        self.label.text=text
-#        self.label.markup=True
-
-#        self.add_widget(self.label)
-#        self.bind(pos=self.change_position)
-#        self.bind(size=self.change_size)
-
-#    def _set_summary_height(self, instance, size):
-#        instance.height = size[1]
-#
-#    def change_position(self, instance, position):
-#        self.background.pos = position
-#
-#    def change_size(self, instance, size):
-#        self.background.size = size
-#        self.label.text_size = (size[0]-50, None)
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import kivy
-from kivy.graphics import Rectangle
-from kivy.uix.widget import Widget
 from kivy_util import ScrollableLabel
-from kivy_util import ImageWithBackground
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
@@ -107,11 +104,10 @@ class Chess_app(App):
         grid = GridLayout(cols = 8, rows = 8, spacing = 0, size_hint=(1, 1))
 
         for i, name in enumerate(SQUARES):
-            bt = Image()
+            bt = Image(allow_stretch=True)
             bt.sq = i
             bt.name = name
-            bt.border = [0,0,0,0]
-#            bt.background_color=(100,0,100,1)
+            # bt.border = [0,0,0,0]
             if i in light_squares:
                 bt.sq_color = "l"
                 bt.background_down = "img/empty-l.png"
