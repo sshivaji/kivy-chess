42061828ae98945e330e03e600128b3cdd9a82c6
Shivkumar Shivaji <sshivaji@gmail.com>
2014-11-15 15:22:43 -0800

    Try with automated install.sh, WIP

---
+++ install.sh
@@ -0,0 +1,6 @@
+python virtualenv.py chesspython
+chesspython/bin/pip install setuptools --no-use-wheel --upgrade
+chesspython/bin/pip install cython==0.20.2
+chesspython/bin/pip install pygame --allow-external pygame --allow-unverified pygame
+chesspython/bin/pip install kivy
+chesspython/bin/pip install -r requirements.txt
