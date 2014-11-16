b4764d2d191918b278f53aa73288eface85cbf1c
sshivaji <sshivaji@gmail.com>
2014-11-15 17:58:12 -0800

    More tweaks to install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
-python virtualenv.py chesspython
+python virtualenv.py --system-site-packages chesspython
 chesspython/bin/pip install setuptools --no-use-wheel --upgrade
 chesspython/bin/pip install cython==0.20.2
-chesspython/bin/pip install pygame --allow-external pygame --allow-unverified pygame
+#chesspython/bin/pip install hg+http://bitbucket.org/pygame/pygame
 chesspython/bin/pip install kivy
+chesspython/bin/pip install git+https://github.com/kivy/buildozer.git@master
+chesspython/bin/pip install git+https://github.com/kivy/plyer.git@master
+chesspython/bin/pip install -U pygments docutils
 chesspython/bin/pip install -r requirements.txt
+git clone https://github.com/jromang/Stockfish
+cd Stockfish
+git checkout pyfish
+cd src
+../../chesspython/bin/python setup.py install
+cd ../..
+#git clone https://github.com/sshivaji/python-chess
+#cd python-chess
+#git checkout pgn
+#../chesspython/bin/python setup.py install
