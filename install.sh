f1f5b03965e642fa45fbf45093c7773552207309
sshivaji <sshivaji@gmail.com>
2016-02-22 00:25:49 -0800

    kivy 1.9.1

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ python virtualenv.py --system-site-packages chesspython
 chesspython/bin/pip install setuptools --no-use-wheel --upgrade
 chesspython/bin/pip install cython==0.20.2
 chesspython/bin/pip install hg+http://bitbucket.org/pygame/pygame
-chesspython/bin/pip install kivy==1.8.0
+chesspython/bin/pip install kivy==1.9.1
 chesspython/bin/pip install git+https://github.com/kivy/buildozer.git@master
 chesspython/bin/pip install git+https://github.com/kivy/plyer.git@master
 chesspython/bin/pip install -U pygments docutils
