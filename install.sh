7aa167687e26224f33904d62e0a56e15179ca904
Shivkumar Shivaji <sshivaji@gmail.com>
2016-11-23 16:41:10 -0800

    fixed scripts

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ chesspython/bin/pip install git+https://github.com/kivy/buildozer.git@master
 chesspython/bin/pip install git+https://github.com/kivy/plyer.git@master
 chesspython/bin/pip install -U pygments docutils
 chesspython/bin/pip install pillow
-chesspython/bin/pip install python-chess==0.12.5
+#chesspython/bin/pip install python-chess==0.12.5
 chesspython/bin/pip install -r requirements.txt
 git clone https://github.com/jromang/Stockfish
 cd Stockfish
