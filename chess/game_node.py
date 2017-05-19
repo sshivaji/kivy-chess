701f5d1d1ab8c81b8102a55ae2ef0051600a9bc8
Shivkumar Shivaji <sshivaji@gmail.com>
2017-05-18 21:54:04 -0700

    simplying and correcting variation output

--- chess/game_node.py
+++ chess/game_node.py
@@ -392,20 +392,20 @@ class GameNode(object):
             for el in n:
                 score = self.process_move(el, figurine, format, q, score)
 
-                for i, v in enumerate(el.__variations):
+                # for i, v in enumerate(el.__variations):
+                #
+                #     if i == 0:
+                #         # Output mainline before variations
+                #         if v.__variations:
+                #             q.append(v.__variations)
+                #             score = self.process_move(v, figurine, format, q, score)
+                #         else:
+                #             q.append([v])
+                #     else:
+                #         q.append([v])
 
-                    if i == 0:
-                        # Output mainline before variations
-                        if v.__variations:
-                            q.append(v.__variations)
-                            score = self.process_move(v, figurine, format, q, score)
-                        else:
-                            q.append([v])
-                    else:
-                        q.append([v])
 
-
-                    # q.append(el.__variations)
+                q.append(el.__variations)
 
                 # print "move: {0}, variations: {1}, length: {2}".format(el.__san, el.__variations, len(el.__variations))
 
