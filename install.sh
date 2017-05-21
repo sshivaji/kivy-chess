eecc50ea1ab80c39ab0ce7635f3e6d3c4f3dc3aa
Shivkumar Shivaji <sshivaji@gmail.com>
2017-05-20 20:41:39 -0700

    modified install for chess_db

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ git clone https://github.com/sshivaji/ctgreader
 cd ctgreader
 sudo make install
 cd ..
-git clone https://github.com/mcostalba/chess_db
+git clone https://github.com/sshivaji/chess_db --branch prod
 cd chess_db/parser
 make build ARCH=x86-64
 sudo make install
